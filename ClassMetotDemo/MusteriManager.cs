c058452de264e50ac4aac08eedca1a5e6dbc782b
Talha GÜLLER <83316373+talhaguller@users.noreply.github.com>
2023-03-14 16:38:13 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi "+musteri.Ad+" "+musteri.Soyad);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.Soyad+" Müşteri Silinmiştir");
+        }
+
+        public void MusteriBİlgileriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşeteri Adı: "+musteri.Ad);
+            Console.WriteLine("Müşeteri Soyadı: " + musteri.Soyad);
+            Console.WriteLine("Müşeteri Tc: " + musteri.Tc);
+
+        }
+
+        public void Lİstele(Musteri[] musteris)
+        {
+            foreach (Musteri musteri in musteris)
+            {
+                Console.WriteLine("Müşeteri Adı: " + musteri.Ad);
+                Console.WriteLine("Müşeteri Soyadı: " + musteri.Soyad);
+                Console.WriteLine("Müşeteri Tc: " + musteri.Tc);
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Talha";
+            musteri1.Soyad = "Güller";
+            musteri1.Tc = "235165165611";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Zeynep";
+            musteri2.Soyad = "Güller";
+            musteri2.Tc = "16116319465616152";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+
+
+            musteriManager.MusteriBİlgileriListele(musteri1);
+
+            Musteri[] musteris = new Musteri[] { musteri1, musteri2 };
+            musteriManager.Lİstele(musteris);
+
+            Console.ReadLine();
+
+        }
+    }
+}
