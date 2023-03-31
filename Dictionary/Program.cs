b8a789aeeaa4aaee6b98d82521df05f43787ca76
Talha GÜLLER <83316373+talhaguller@users.noreply.github.com>
2023-03-31 11:42:14 +0300

    Kodlamaio C# 4Gün 5Ödev

---
+++ Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> dene = new MyDictionary<int, string>();
+            dene.Add(1, "Ahmet");
+            dene.Add(2, "mehmet");
+            dene.Add(2, "mehmet");
+            Console.WriteLine(dene.Length);
+
+            for (int i = 0; i < dene.Length; i++)
+            {
+                Console.WriteLine("id: {0} - isim:{1} ", dene.GetKeys[i], dene.GetValues[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
